eca348146ae38d5270d9241265f90c90b95907b2
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2022-10-05 00:02:26 +0300

    Add LoadingButton

---
+++ CodeBeam.MudExtensions/Enums/ButtonVariant.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MudExtensions
+{
+    public enum ButtonVariant
+    {
+        [Description("button")]
+        Button,
+        [Description("iconbutton")]
+        IconButton,
+        [Description("fab")]
+        Fab
+    }
+}
