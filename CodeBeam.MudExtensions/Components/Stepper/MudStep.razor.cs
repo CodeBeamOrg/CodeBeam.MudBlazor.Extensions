be32921bced69d4f5a4c5fa0b57fc5e34f3f0e73
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2022-10-09 18:59:35 +0300

    Initialize

---
+++ CodeBeam.MudExtensions/Components/Stepper/MudStep.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudExtensions
+{
+    public partial class MudStep : MudComponentBase
+    {
+        [CascadingParameter]
+        protected MudStepper MudStepper { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            MudStepper.AddStep(this);
+        }
+    }
+}
---
+++ CodeBeam.MudExtensions/Components/Stepper/MudStepper.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudExtensions
+{
+    public partial class MudStepper : MudComponentBase
+    {
+        [Parameter]
+        public int ActiveIndex { get; set; }
+
+        [Parameter]
+        public bool Vertical { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        List<MudStep> _steps = new();
+
+        public List<MudStep> Steps 
+        { 
+            get => _steps; 
+            protected set
+            {
+                if (_steps.Equals(value))
+                {
+                    return;
+                }
+                _steps = value;
+            }
+        }
+
+        internal void AddStep(MudStep step)
+        {
+            _steps.Add(step);
+
+            StateHasChanged();
+        }
+
+        protected void SetActiveIndex(MudStep step)
+        {
+            ActiveIndex = Steps.IndexOf(step);
+        }
+    }
+}
