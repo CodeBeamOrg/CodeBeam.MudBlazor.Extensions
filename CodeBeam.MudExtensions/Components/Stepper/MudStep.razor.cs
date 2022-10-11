829f6e7674aeba583ed8196e039b105806dcabc7
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2022-10-11 23:45:58 +0300

    Completed Parameter

--- CodeBeam.MudExtensions/Components/Stepper/MudStep.razor.cs
+++ CodeBeam.MudExtensions/Components/Stepper/MudStep.razor.cs
@@ -8,13 +8,20 @@ using System.Threading.Tasks;
 
 namespace MudExtensions
 {
-    public partial class MudStep : MudComponentBase
+    public partial class MudStep : MudComponentBase, IDisposable
     {
+
         [CascadingParameter]
         protected MudStepper MudStepper { get; set; }
 
         [Parameter]
         public string Title { get; set; }
+
+        /// <summary>
+        /// If true the requirement of the step is completed and shows an tick into the header.
+        /// </summary>
+        [Parameter]
+        public bool Completed { get; set; }
 
         /// <summary>
         /// If true the step is skippable.
@@ -23,12 +30,33 @@ namespace MudExtensions
         public bool Optional { get; set; }
 
         [Parameter]
+        public bool ResultStep { get; set; }
+
+        [Parameter]
         public RenderFragment ChildContent { get; set; }
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
-            MudStepper.AddStep(this);
+            if (ResultStep == false)
+            {
+                MudStepper.AddStep(this);
+            }
         }
+
+        protected internal void SetComplete(bool value)
+        {
+            Completed = value;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                MudStepper?.RemoveStep(this);
+            }
+            catch (Exception) { }
+        }
+
     }
 }
--- CodeBeam.MudExtensions/Components/Stepper/MudStepper.razor.cs
+++ CodeBeam.MudExtensions/Components/Stepper/MudStepper.razor.cs
@@ -1,17 +1,30 @@
 ﻿using CodeBeam.MudExtensions.Utilities;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using MudBlazor.Extensions;
+using MudBlazor.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static MudBlazor.Colors;
 
 namespace MudExtensions
 {
     public partial class MudStepper : MudComponentBase
     {
         MudAnimate _animate;
+
+        protected string HeaderClassname => new CssBuilder("d-flex align-center mud-stepper-header gap-4 pa-2")
+            .AddClass("mud-ripple", DisableRipple == false && Linear == false)
+            .AddClass("cursor-pointer", Linear == false)
+            .Build();
+
+        protected string DashClassname => new CssBuilder("mud-stepper-header-dash flex-grow-1 mx-auto")
+            .AddClass("mud-stepper-header-dash-completed")
+            .Build();
 
         [Parameter]
         public int ActiveIndex { get; set; }
@@ -27,6 +40,12 @@ namespace MudExtensions
         /// </summary>
         [Parameter]
         public bool DisableHeaderText { get; set; }
+
+        /// <summary>
+        /// If true, disabled ripple effect when click on step headers.
+        /// </summary>
+        [Parameter]
+        public bool DisableRipple { get; set; }
 
         [Parameter]
         public Color Color { get; set; } = Color.Default;
@@ -44,6 +63,9 @@ namespace MudExtensions
         public RenderFragment ChildContent { get; set; }
 
         [Parameter]
+        public RenderFragment CompletedContent { get; set; }
+
+        [Parameter]
         public EventCallback<int> ActiveStepChanged { get; set; }
 
         List<MudStep> _steps = new();
@@ -57,6 +79,10 @@ namespace MudExtensions
                 {
                     return;
                 }
+                if (_steps.Select(x => x.GetHashCode()).Contains(value.GetHashCode()))
+                {
+                    return;
+                }
                 _steps = value;
             }
         }
@@ -65,32 +91,71 @@ namespace MudExtensions
         {
             _steps.Add(step);
 
+            //Steps.Add(step);
+            //Steps = Steps;
+
+            StateHasChanged();
+        }
+
+        internal void RemoveStep(MudStep step)
+        {
+            Steps.Remove(step);
+
             StateHasChanged();
         }
 
         protected async Task SetActiveIndex(MudStep step)
         {
             ActiveIndex = Steps.IndexOf(step);
+            _isResultStep = false;
             await ActiveStepChanged.InvokeAsync();
             await _animate.Refresh();
         }
 
         protected async Task SetActiveIndex(int count)
         {
+            if (_isResultStep)
+            {
+                ActiveIndex = Steps.Count;
+            }
             if (Steps.Count - 1 < ActiveIndex + count)
             {
                 ActiveIndex = Steps.Count - 1;
+                if (IsAllStepsCompleted() == false)
+                {
+                    _isResultStep = true;
+                }
             }
             else if (ActiveIndex + count < 0)
             {
                 ActiveIndex = 0;
+                _isResultStep = false;
             }
             else
             {
-                ActiveIndex += count;
+                if (IsAllStepsCompleted())
+                {
+                    ActiveIndex = Steps.Count - 1;
+                    _isResultStep = true;
+                }
+                else
+                {
+                    ActiveIndex += count;
+                    _isResultStep = false;
+                }
             }
             await ActiveStepChanged.InvokeAsync();
             await _animate.Refresh();
+        }
+
+        internal bool _isResultStep = false;
+        public async Task CompleteStep(int index, bool moveToNextStep = true)
+        {
+            Steps[index].SetComplete(true);
+            if (moveToNextStep)
+            {
+                await SetActiveIndex(1);
+            }
         }
 
         protected bool IsStepActive(MudStep step)
@@ -98,9 +163,19 @@ namespace MudExtensions
             return Steps.IndexOf(step) == ActiveIndex;
         }
 
+        public bool IsAllStepsCompleted()
+        {
+            return !Steps.Any(x => x.Completed == false && x.Optional == false);
+        }
+
+        protected int CompletedStepCount()
+        {
+            return Steps.Where(x => x.Completed).Count();
+        }
+
         protected string GetNextButtonString()
         {
-            if (Steps.Count - 1 == ActiveIndex)
+            if (Steps.Count - 1 == CompletedStepCount())
             {
                 return LocalizedStrings.Finish;
             }
@@ -110,12 +185,11 @@ namespace MudExtensions
             }
         }
 
-        protected void UpdateAvatars()
+        public void Reset()
         {
-            foreach (MudStep step in Steps)
-            {
-
-            }
+            Steps.ForEach(x => x.SetComplete(false));
+            ActiveIndex = 0;
+            _isResultStep = false;
         }
 
     }
