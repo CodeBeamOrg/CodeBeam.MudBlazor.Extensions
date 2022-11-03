b10c040ee3e54b7f5fa5b64d247e40ba7293f7be
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2022-11-03 19:41:23 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh 
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
+chmod +x dotnet-install.sh 
+./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6 
+./dotnet6/dotnet --version 
+./dotnet6/dotnet publish -c Release -o output
