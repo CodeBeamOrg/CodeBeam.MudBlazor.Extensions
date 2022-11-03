8c434892a14fe75b1ffb0c1aad238452436aa154
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2022-11-03 19:39:31 +0300

    Update build.sh

--- ComponentViewer.Wasm/build.sh
+++ ComponentViewer.Wasm/build.sh
@@ -1,5 +1,6 @@
-#!/bin/sh curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
+#!/bin/sh 
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
 chmod +x dotnet-install.sh 
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6 
 ./dotnet6/dotnet --version 
-./dotnet6/dotnet publish src/ComponentViewer.Wasm/ComponentViewer.Wasm.csproj -c Release -o output
+./dotnet6/dotnet publish -c Release -o output
