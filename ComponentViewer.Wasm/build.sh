87ca46f21396d67c027b0c73166c891eddccbab1
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2022-11-05 02:03:38 +0300

    Delete build.sh

--- ComponentViewer.Wasm/build.sh
+++
@@ -1,5 +0,0 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
-chmod +x dotnet-install.sh 
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6 
-./dotnet6/dotnet --version 
-./dotnet6/dotnet publish src/ComponentViewer.Wasm/ComponentViewer.Wasm.csproj -c Release -o output
