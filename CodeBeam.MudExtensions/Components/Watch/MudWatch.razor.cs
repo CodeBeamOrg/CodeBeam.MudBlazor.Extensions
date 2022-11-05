e4e7ec1f795ea29feb1a69cc40a5b07edc988c2b
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2022-11-05 14:39:03 +0300

    Cleanup

--- CodeBeam.MudExtensions/Components/Watch/MudWatch.razor.cs
+++ CodeBeam.MudExtensions/Components/Watch/MudWatch.razor.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Data;
 using System.Diagnostics;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Timers;
 using CodeBeam.MudExtensions.Utilities;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.DependencyInjection;
 using MudBlazor;
 using MudBlazor.Utilities;
 using MudExtensions.Enums;
@@ -15,8 +10,13 @@ using static MudBlazor.CategoryTypes;
 
 namespace MudExtensions
 {
-    public partial class MudWatch : MudBaseInput<TimeSpan?>
+    public partial class MudWatch : MudComponentBase
     {
+
+        protected string Classname => new CssBuilder("d-flex mud-width-full align-end justify-center")
+            .AddClass(Class)
+            .Build();
+
         MudWheel<int> _wheelHour;
         MudWheel<int> _wheelMinute;
         MudWheel<int> _wheelSecond;
@@ -24,127 +24,6 @@ namespace MudExtensions
         System.Timers.Timer _timer = new();
         Stopwatch _stopwatch = new();
 
-        public async void Elapse(object sender, System.Timers.ElapsedEventArgs args)
-        {
-            int oldHour = ((int)Value.Value.TotalHours);
-            int oldMinute = ((int)Value.Value.TotalMinutes);
-            int oldSecond = ((int)Value.Value.TotalSeconds);
-            if (Mode == WatchMode.Watch)
-            {
-                Value = DateTime.Now.TimeOfDay;
-                SetInternalValues();
-            }
-            else if (Mode == WatchMode.CountDown)
-            {
-                if (Value.Value <= TimeSpan.Zero)
-                {
-                    await Stop();
-                    Value = TimeSpan.Zero;
-                    SetInternalValues();
-                    await InvokeAsync(StateHasChanged);
-                    await InvokeAsync(CountdownCompleted.InvokeAsync);
-                    return;
-                }
-
-                int oldSecondValue = ((int)Value.Value.TotalSeconds);
-                Value = CountdownTime - TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
-                SetInternalValues();
-                
-            }
-            else
-            {
-                int oldSecondValue = ((int)Value.Value.TotalSeconds);
-                Value = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
-                SetInternalValues();
-            }
-#pragma warning disable CS4014
-            if (Wheel == true)
-            {
-                if (oldHour != ((int)Value.Value.TotalHours))
-                {
-                    // When you await this line, some animates will miss.
-                    _wheelHour.RefreshAnimate();
-                }
-
-                if (oldMinute != ((int)Value.Value.TotalMinutes))
-                {
-                    // When you await this line, some animates will miss.
-                    _wheelSecond.RefreshAnimate();
-                }
-
-                if (oldSecond != ((int)Value.Value.TotalSeconds))
-                {
-                    // When you await this line, some animates will miss.
-                    _wheelSecond.RefreshAnimate();
-                }
-            }
-#pragma warning restore CS4014
-            await InvokeAsync(StateHasChanged);
-        }
-
-        public async Task Stop()
-        {
-            _timer.Stop();
-            _stopwatch.Stop();
-            if (Mode == WatchMode.CountDown)
-            {
-                Value = CountdownTime - TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
-            }
-            else if (Mode == WatchMode.StopWatch)
-            {
-                Value = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
-            }
-
-            SetInternalValues();
-            await InvokeAsync(StateHasChanged);
-        }
-
-        public void ResetWatch()
-        {
-            if (Mode == WatchMode.Watch)
-            {
-                return;
-            }
-            else if (Mode == WatchMode.CountDown)
-            {
-                _stopwatch.Reset();
-                Value = CountdownTime;
-                SetInternalValues();
-                LapRecords.Clear();
-                return;
-            }
-            _stopwatch.Reset();
-            Value = TimeSpan.Zero;
-            SetInternalValues();
-            LapRecords.Clear();
-        }
-
-        public void Start()
-        {
-            _timer.Interval = Interval.TotalMilliseconds;
-            _timer.Start();
-            _stopwatch.Start();
-        }
-
-        public async void Lap()
-        {
-            if (LapRecords == null)
-            {
-                LapRecords = new();
-            }
-            LapRecords.Add(new LapRecord()
-            {
-                TotalTime = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds),
-                Gap = LapRecords.Count == 0 ? TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds) : TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds) - LapRecords.LastOrDefault().TotalTime,
-            });
-            await LapRecordsChanged.InvokeAsync();
-        }
-
-        protected string Classname =>
-           new CssBuilder("mud-input-input-control")
-           .AddClass(Class)
-           .Build();
-        TimeSpan dt;
         protected override void OnInitialized()
         {
             _timer.Elapsed += Elapse;
@@ -160,11 +39,6 @@ namespace MudExtensions
             {
                 Value = TimeSpan.FromSeconds(0);
             }
-            //Converter = new MudBlazor.Converter<TimeSpan?, string>()
-            //{
-            //    SetFunc = x => x?.ToString(),
-            //    GetFunc = x => TimeSpan.TryParseExact(x, DateFormat, null, System.Globalization.TimeSpanStyles.None, out dt) == true ? dt : null,
-            //};
 
             if (Mode == WatchMode.Watch)
             {
@@ -173,18 +47,38 @@ namespace MudExtensions
             }
         }
 
-        public MudInput<string> InputReference { get; private set; }
+        TimeSpan _value;
+        [Parameter]
+        public TimeSpan Value 
+        {
+            get => _value; 
+            set
+            {
+                if (_value == value)
+                {
+                    return;
+                }
+                _value = value;
+                SetInternalValues();
+            }
+        }
 
-        [CascadingParameter(Name = "Standalone")]
-        internal bool StandaloneEx { get; set; } = true;
-
+        TimeSpan _interval = TimeSpan.FromSeconds(1);
         [Parameter]
         [Category(CategoryTypes.FormComponent.Behavior)]
-        public TimeSpan Interval { get; set; } = TimeSpan.FromMilliseconds(100);
-
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Behavior)]
-        public TimeSpan Increment { get; set; } = TimeSpan.FromMilliseconds(100);
+        public TimeSpan Interval 
+        { 
+            get => _interval; 
+            set
+            {
+                if (_interval == value)
+                {
+                    return;
+                }
+                _interval = value;
+                _timer.Interval = _interval.TotalMilliseconds;
+            }
+        }
 
         WatchMode _watchMode = WatchMode.Watch;
         [Parameter]
@@ -264,6 +158,13 @@ namespace MudExtensions
         public Typo TypoMillisecond { get; set; } = Typo.h6;
 
         /// <summary>
+        /// Fires when value changed.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.FormComponent.Behavior)]
+        public EventCallback ValueChanged { get; set; }
+
+        /// <summary>
         /// Fires when countdown reach to 0.
         /// </summary>
         [Parameter]
@@ -305,31 +206,121 @@ namespace MudExtensions
         [Category(CategoryTypes.FormComponent.ListBehavior)]
         public List<LapRecord> LapRecords { get; set; } = new();
 
+        /// <summary>
+        /// Fires when new lap record added.
+        /// </summary>
         [Parameter]
         [Category(CategoryTypes.FormComponent.ListBehavior)]
         public EventCallback<LapRecord> LapRecordsChanged { get; set; }
 
-        /// <summary>
-        /// Button click event for clear button. Called after text and value has been cleared.
-        /// </summary>
-        [Parameter] public EventCallback<MouseEventArgs> OnClearButtonClick { get; set; }
-
-        protected async Task UpdateValueAsync(bool updateText = true)
+        public async void Elapse(object sender, System.Timers.ElapsedEventArgs args)
         {
-            var _backUpValue = Value;
-            try
+            int oldHour = ((int)Value.TotalHours);
+            int oldMinute = ((int)Value.TotalMinutes);
+            int oldSecond = ((int)Value.TotalSeconds);
+            if (Mode == WatchMode.Watch)
             {
-                await SetValueAsync(new TimeSpan(_hour, _minute, _second), updateText);
+                Value = DateTime.Now.TimeOfDay;
             }
-            catch
+            else if (Mode == WatchMode.CountDown)
             {
-                await SetValueAsync(_backUpValue, updateText);
-            }
-        }  
+                if (Value <= TimeSpan.Zero)
+                {
+                    await Stop();
+                    Value = TimeSpan.Zero;
+                    await InvokeAsync(StateHasChanged);
+                    await InvokeAsync(CountdownCompleted.InvokeAsync);
+                    return;
+                }
 
-        protected async Task HandleOnBlur()
+                int oldSecondValue = ((int)Value.TotalSeconds);
+                Value = CountdownTime - TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+            }
+            else
+            {
+                int oldSecondValue = ((int)Value.TotalSeconds);
+                Value = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+            }
+#pragma warning disable CS4014
+            if (Wheel == true)
+            {
+                if (oldHour != ((int)Value.TotalHours))
+                {
+                    // When you await this line, some animates will miss.
+                    _wheelHour.RefreshAnimate();
+                }
+
+                if (oldMinute != ((int)Value.TotalMinutes))
+                {
+                    // When you await this line, some animates will miss.
+                    _wheelSecond.RefreshAnimate();
+                }
+
+                if (oldSecond != ((int)Value.TotalSeconds))
+                {
+                    // When you await this line, some animates will miss.
+                    _wheelSecond.RefreshAnimate();
+                }
+            }
+#pragma warning restore CS4014
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public async Task Stop()
         {
-            await SetTextAsync(InputReference.Text, true);
+            _timer.Stop();
+            _stopwatch.Stop();
+            if (Mode == WatchMode.CountDown)
+            {
+                Value = CountdownTime - TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+            }
+            else if (Mode == WatchMode.StopWatch)
+            {
+                Value = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public async void Reset()
+        {
+            if (Mode == WatchMode.Watch)
+            {
+                return;
+            }
+            else if (Mode == WatchMode.CountDown)
+            {
+                _stopwatch.Reset();
+                Value = CountdownTime;
+                LapRecords.Clear();
+                await LapRecordsChanged.InvokeAsync();
+                return;
+            }
+            _stopwatch.Reset();
+            Value = TimeSpan.Zero;
+            LapRecords.Clear();
+            await LapRecordsChanged.InvokeAsync();
+        }
+
+        public void Start()
+        {
+            _timer.Interval = Interval.TotalMilliseconds;
+            _timer.Start();
+            _stopwatch.Start();
+        }
+
+        public async void Lap()
+        {
+            if (LapRecords == null)
+            {
+                LapRecords = new();
+            }
+            LapRecords.Add(new LapRecord()
+            {
+                TotalTime = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds),
+                Gap = LapRecords.Count == 0 ? TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds) : TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds) - LapRecords.LastOrDefault().TotalTime,
+            });
+            await LapRecordsChanged.InvokeAsync();
         }
 
         protected void SetWatchMode(WatchMode mode)
@@ -347,7 +338,7 @@ namespace MudExtensions
             {
                 Interval = TimeSpan.FromMilliseconds(1);
                 Value = TimeSpan.Zero;
-                ResetWatch();
+                Reset();
                 ShowHour = true;
                 ShowMinute = true;
                 ShowSecond = true;
@@ -356,7 +347,7 @@ namespace MudExtensions
             else if (mode == WatchMode.CountDown)
             {
                 Interval = TimeSpan.FromMilliseconds(1);
-                ResetWatch();
+                Reset();
                 ShowHour = true;
                 ShowMinute = true;
                 ShowSecond = true;
@@ -366,20 +357,10 @@ namespace MudExtensions
 
         protected void SetInternalValues()
         {
-            if (Value == null)
-            {
-                return;
-            }
-            _hour = Value.Value.Hours;
-            _minute = Value.Value.Minutes;
-            _second = Value.Value.Seconds;
-            _milliSecond = Value.Value.Milliseconds;
-        }
-
-        protected override void ResetValue()
-        {
-            InputReference.Reset();
-            base.ResetValue();
+            _hour = Value.Hours;
+            _minute = Value.Minutes;
+            _second = Value.Seconds;
+            _milliSecond = Value.Milliseconds;
         }
 
         protected string GetWatchText()
@@ -411,33 +392,6 @@ namespace MudExtensions
                 return $"0{val}";
             }
             return val.ToString();
-        }
-
-        /// <summary>
-        /// Clear the text field, set Value to default(T) and Text to null
-        /// </summary>
-        /// <returns></returns>
-        public async Task Clear()
-        {
-            await SetValueAsync(null);
-            await InputReference.SetText(null);
-        }
-
-        protected async Task HandleClearButtonClick()
-        {
-            await Clear();
-            await OnClearButtonClick.InvokeAsync();
-        }
-
-        /// <summary>
-        /// Sets the input text from outside programmatically
-        /// </summary>
-        /// <param name="text"></param>
-        /// <returns></returns>
-        public async Task SetText(string text)
-        {
-            if (InputReference != null)
-                await InputReference.SetText(text);
         }
 
     }
