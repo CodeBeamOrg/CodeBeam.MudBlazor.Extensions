27a71681e258ea86455549e4c6e996eb1e0c0a90
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2022-11-07 00:27:34 +0300

    TestProject (#44)

---
+++ CodeBeam.MudExtensions.UnitTest/Components/StepperTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using CodeBeam.MudExtensions.UnitTests.Components;
+using FluentAssertions;
+using MudBlazor;
+using MudExtensions;
+
+namespace CodeBeam.MudExtensions.UnitTest.Components
+{
+    [TestFixture]
+    public class StepperTests : BunitTest
+    {
+        [Test]
+        public void StepperRenderTest()
+        {
+            var comp = Context.RenderComponent<MudStepper>();
+            comp.Instance.Steps.Count.Should().Be(0);
+        }
+    }
+}
---
+++ CodeBeam.MudExtensions.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodeBeam.MudExtensions.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
