18bf4b9877bbb0c930b407847aad31a97556c2a3
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2023-01-22 23:21:28 +0300

    AddMudExtensions for the services

--- ComponentViewer.Wasm/Program.cs
+++ ComponentViewer.Wasm/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
-builder.Services.AddScoped<IScrollManagerExtended, ScrollManagerExtended>();
+builder.Services.AddMudExtensions();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- ComponentViewer/Program.cs
+++ ComponentViewer/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
-builder.Services.AddScoped<IScrollManagerExtended, ScrollManagerExtended>();
+builder.Services.AddMudExtensions();
 
 var app = builder.Build();
 
