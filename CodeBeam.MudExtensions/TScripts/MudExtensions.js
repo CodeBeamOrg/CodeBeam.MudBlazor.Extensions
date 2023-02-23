4106e272a695ffbf4ad12845757e7b814cff12ec
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2023-02-23 17:59:01 +0300

    MinifyJs

--- CodeBeam.MudExtensions/TScripts/MudExtensions.js
+++ CodeBeam.MudExtensions/TScripts/MudExtensions.js
@@ -1,12 +1,17 @@
 ﻿
-//class MudScrollManagerExtended {
+class MudScrollManagerExtended {
 
-//    scrollToMiddle(parentId, childId) {
+    scrollToMiddle(parentId, childId) {
 
-//        let parent = document.getElementById(parentId);
-//        let child = document.getElementById(childId);
+        let parent = document.getElementById(parentId);
+        let child = document.getElementById(childId);
 
-//        parent.scrollTop = (child.offsetTop - parent.offsetHeight) + (parent.offsetHeight / 2) + (child.offsetHeight / 2);
-//    }
-//};
-//window.mudScrollManagerExtended = new MudScrollManagerExtended();
+        parent.scrollTop = (child.offsetTop - parent.offsetHeight) + (parent.offsetHeight / 2) + (child.offsetHeight / 2);
+    }
+};
+window.mudScrollManagerExtended = new MudScrollManagerExtended();
+
+function auto_size(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight + 4) + "px";
+}
