60f075e91cf1a2b3226cd4fc6781c5b3f9c9f678
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2023-04-11 00:39:49 +0300

    Basic Start of QrGenerator

---
+++ CodeBeam.MudBlazor.Extensions/Components/QrGenerator/MudQrGenerator.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SkiaSharp;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.SkiaSharp;
+
+namespace MudExtensions
+{
+    public partial class MudQrGenerator : MudComponentBase
+    {
+        string _content = "";
+
+        public byte[] Value { get; set; }
+
+        public void Refresh(string value)
+        {
+            _content = value;
+            Value = CreateQrCode(value);
+            StateHasChanged();
+        }
+
+        public byte[] CreateQrCode(string content)
+        {
+            BarcodeWriter writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Width = 100,
+                    Height = 100,
+                }
+            };
+
+            var qrCodeImage = writer.Write(content);
+
+            using (var stream = new MemoryStream())
+            {
+                qrCodeImage.Encode(stream, SKEncodedImageFormat.Png, 100);
+                return stream.ToArray();
+            }
+        }
+    }
+}
