31cdfd9092c75ce46c6b3509455fef2c49c52a63
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2023-04-24 13:51:13 +0300

    MudTeleport

---
+++ CodeBeam.MudBlazor.Extensions/Components/Teleport/MudTeleport.razor.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+using MudBlazor.Utilities;
+using MudExtensions.Utilities;
+
+namespace MudExtensions
+{
+    public partial class MudTeleport : MudComponentBase
+    {
+        [Inject] MudTeleportManager MudTeleportManager { get; set; }
+
+        private string _generatedClass = "teleport" + Guid.NewGuid().ToString().Substring(0, 8);
+
+        protected string Classname => new CssBuilder()
+            .AddClass(_generatedClass)
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        /// The class name that shows the parent which the teleport content will teleport.
+        /// </summary>
+        [Parameter] public string To { get; set; }
+
+        /// <summary>
+        /// The class name that used to return teleport content to the container.
+        /// </summary>
+        [Parameter] public string OwnClass { get; set; }
+
+        /// <summary>
+        /// If true the content never show on container. Default is false.
+        /// </summary>
+        [Parameter] public bool HideOnContainer { get; set; }
+
+        /// <summary>
+        /// If true teleported content returns the container, otherwise the content remains the last teleported place. Default is false.
+        /// </summary>
+        [Parameter] public bool ReturnWhenNotFound { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private ElementReference _ref;
+
+        private string _to;
+        private bool _mustUpdate = true;
+        private string _lastUpdatedState = "not found";
+
+        protected override void OnParametersSet()
+        {
+            // if `To` or `Disabled` has changed we must update the teleport
+            if (To != null && (!To.Equals(_to)))
+            {
+                _to = To;
+                _mustUpdate = true;
+            }
+            else
+            {
+                _mustUpdate = false;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (_mustUpdate)
+            {
+                await Update();
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected bool ShouldShow()
+        {
+            if (HideOnContainer == true && _lastUpdatedState != "ok")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task Update()
+        {
+            var result = await MudTeleportManager.Teleport(_ref, To);
+            if (result == "not found" && ReturnWhenNotFound == true)
+            {
+                await MudTeleportManager.Teleport(_ref, _generatedClass);
+            }
+            _lastUpdatedState = result;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            try
+            {
+                await MudTeleportManager.RemoveFromDom(_ref);
+            }
+            catch (JSDisconnectedException) { }
+            catch (TaskCanceledException) { }
+            catch (InvalidOperationException) { /* it throws in the server side project */ }
+        }
+    }
+}
