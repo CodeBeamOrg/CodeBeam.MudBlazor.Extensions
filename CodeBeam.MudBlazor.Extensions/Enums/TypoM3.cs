483f982cfebbef3c0cc68de589381c95bae700d9
segfault- <david.luyk@gmail.com>
2023-07-06 14:56:57 -0400

    [New Component] - MudTextM3 (#243)
    * no message
    
    * no message
    
    * Initial creation of MudTextM3 based on material 3 typography specifications - https://m3.material.io/styles/typography/overview
    
    * missed scss update
    
    * Updates based on review
    
    ---------
    
    Co-authored-by: segfault <five90-segfault@outlook.com>

---
+++ CodeBeam.MudBlazor.Extensions/Enums/TypoM3.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace MudExtensions.Enums
+{
+    public enum TypoM3
+    {
+        [Description("Display")]
+        Display,
+        [Description("Headline")]
+        Headline,
+        [Description("Title")]
+        Title,
+        [Description("Body")]
+        Body,
+        [Description("Label")]
+        Label
+    }
+}
