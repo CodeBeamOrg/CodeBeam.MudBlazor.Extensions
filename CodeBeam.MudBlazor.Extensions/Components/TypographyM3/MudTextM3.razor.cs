53efdd05a81a1b131a4dcd451926b2ea24ffb14d
marcoatribeiro <35035369+marcoatribeiro@users.noreply.github.com>
2023-07-29 09:57:11 -0300

    Added other MudText properties to MudTextM3 (#252)
    * Added Align and Color properties to MudTextM3
    
    * Added other properties and fixed class settings
    
    ---------
    
    Co-authored-by: Marco <marco@basicondo.com.br>

--- CodeBeam.MudBlazor.Extensions/Components/TypographyM3/MudTextM3.razor.cs
+++ CodeBeam.MudBlazor.Extensions/Components/TypographyM3/MudTextM3.razor.cs
@@ -11,12 +11,29 @@ namespace MudExtensions
     {
         protected string ClassName => new CssBuilder("mud-typographym3")
             .AddClass($"mud-typographym3-{Typo.ToDescriptionString()}-{Size.ToDescriptionString()}") // .mud-typographym3-#{$style}-#{$size}
+            .AddClass($"mud-{Color.ToDescriptionString()}-text", Color != Color.Default && Color != Color.Inherit)
+            .AddClass("mud-typography-gutterbottom", GutterBottom)
+            .AddClass($"mud-typography-align-{ConvertAlign(Align).ToDescriptionString()}", Align != Align.Inherit)
+            .AddClass("mud-typography-display-inline", Inline)
             .AddClass(Class)
             .Build();
 
         protected string StyleString => new StyleBuilder()
             .AddStyle(Style)
             .Build();
+
+        private Align ConvertAlign(Align align)
+        {
+            return align switch
+            {
+                Align.Start => RightToLeft ? Align.Right : Align.Left,
+                Align.End => RightToLeft ? Align.Left : Align.Right,
+                _ => align
+            };
+        }
+
+        [CascadingParameter(Name = "RightToLeft")]
+        public bool RightToLeft { get; set; }
 
         /// <summary>
         /// Set the text-align on the component.
@@ -37,5 +54,33 @@ namespace MudExtensions
         [Parameter]
         [Category(CategoryTypes.Text.Behavior)]
         public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// Set the text-align on the component.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Text.Appearance)]
+        public Align Align { get; set; } = Align.Inherit;
+
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Text.Appearance)]
+        public Color Color { get; set; } = Color.Inherit;
+
+        /// <summary>
+        /// If true, the text will have a bottom margin.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Text.Appearance)]
+        public bool GutterBottom { get; set; } = false;
+
+        /// <summary>
+        /// If true, Sets display inline
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Text.Appearance)]
+        public bool Inline { get; set; }
     }
 }
