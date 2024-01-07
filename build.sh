7459637e57c7faf969646b0b7d88a33f283420c4
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2024-01-08 01:29:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
 chmod +x dotnet-install.sh 
-./dotnet-install.sh -c 7.0 --version 7.0.203 -InstallDir ./dotnet7 
-./dotnet7/dotnet --version 
-./dotnet7/dotnet tool install Excubo.WebCompiler --global
-./dotnet7/dotnet publish ComponentViewer.Wasm/ComponentViewer.Wasm.csproj -c Release -o output
+./dotnet-install.sh -c 8.0 --version 8.0.100 -InstallDir ./dotnet8 
+./dotnet8/dotnet --version 
+./dotnet8/dotnet tool install Excubo.WebCompiler --global
+./dotnet8/dotnet publish ComponentViewer.Wasm/ComponentViewer.Wasm.csproj -c Release -o output
