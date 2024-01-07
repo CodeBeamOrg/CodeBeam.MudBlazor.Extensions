ba54fb9ebf85fee335f5a137e32b853bb2c18d6b
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2024-01-08 00:16:06 +0300

    .NET 8 Support

--- CodeBeam.MudBlazor.Extensions.UnitTests/Mocks/MockJsApiServices.cs
+++ CodeBeam.MudBlazor.Extensions.UnitTests/Mocks/MockJsApiServices.cs
@@ -6,12 +6,14 @@ namespace MudExtensions.UnitTests.Mocks
     /// <summary>
     /// Mock for scroll listener
     /// </summary>
-    public class MockJsApiServices : IJsApiService
-    {
-        public ValueTask CopyToClipboardAsync(string text) => ValueTask.CompletedTask;
+    //public class MockJsApiService : IJsApiService
+    //{
+    //    public ValueTask CopyToClipboardAsync(string text) => ValueTask.CompletedTask;
 
-        public ValueTask Open(string link, string target) => ValueTask.CompletedTask;
+    //    public ValueTask Open(string link, string target) => ValueTask.CompletedTask;
 
-        public ValueTask OpenInNewTabAsync(string url) => ValueTask.CompletedTask;
-    }
+    //    public ValueTask UpdateStyleProperty(string elementId, string propertyName, object value) => ValueTask.CompletedTask;
+
+    //    public ValueTask OpenInNewTabAsync(string url) => ValueTask.CompletedTask;
+    //}
 }
