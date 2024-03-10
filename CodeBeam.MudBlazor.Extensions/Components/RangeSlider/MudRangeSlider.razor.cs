26fe2628ecdb0e90eab60711d45a72325c0d3919
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2024-03-10 23:09:09 +0300

    RangeSlider Customizable Value Labels (#360)

--- CodeBeam.MudBlazor.Extensions/Components/RangeSlider/MudRangeSlider.razor.cs
+++ CodeBeam.MudBlazor.Extensions/Components/RangeSlider/MudRangeSlider.razor.cs
@@ -45,6 +45,20 @@ namespace MudExtensions
         }
 
         /// <summary>
+        /// Custom text for ValueLabel
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Slider.Validation)]
+        public string? LabelText { get; set; }
+
+        /// <summary>
+        /// Custom text for upper ValueLabel
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Slider.Validation)]
+        public string? UpperLabelText { get; set; }
+
+        /// <summary>
         /// The minimum allowed value of the slider. Should not be equal to max.
         /// </summary>
         [Parameter]
