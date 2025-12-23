b17b265f695cecb7e82dbaf69127f66a9ad821c7
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2025-12-23 22:18:35 +0300

    Change Scss and Js Compilers & Remove All Dependencies Except MudBlazor

---
+++ CodeBeam.MudBlazor.Extensions.JsMinifier/Program.cs
@@ -0,0 +1,38 @@
+﻿using NUglify;
+using System.Text;
+
+Console.WriteLine("Minifying MudExtensions.js...");
+
+// Solution root
+var solutionRoot = Directory.GetCurrentDirectory();
+
+// Ana proje klasörü
+var projectRoot = Path.Combine(solutionRoot, "CodeBeam.MudBlazor.Extensions");
+
+var input = Path.Combine(projectRoot, "TScripts", "MudExtensions.js");
+var output = Path.Combine(projectRoot, "wwwroot", "MudExtensions.min.js");
+
+Console.WriteLine($"IN : {input}");
+Console.WriteLine($"OUT: {output}");
+
+if (!File.Exists(input))
+{
+    Console.Error.WriteLine($"Input file not found: {input}");
+    Environment.Exit(1);
+}
+
+var js = File.ReadAllText(input, Encoding.UTF8);
+var result = Uglify.Js(js);
+
+if (result.HasErrors)
+{
+    foreach (var error in result.Errors)
+        Console.Error.WriteLine(error.ToString());
+
+    Environment.Exit(1);
+}
+
+Directory.CreateDirectory(Path.GetDirectoryName(output)!);
+File.WriteAllText(output, result.Code!, Encoding.UTF8);
+
+Console.WriteLine("✔ MudExtensions.min.js generated successfully");
