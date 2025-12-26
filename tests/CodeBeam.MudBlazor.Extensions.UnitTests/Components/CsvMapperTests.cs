52ca9f81b9f74357187626519db73c906929c1cb
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2025-12-27 00:49:56 +0300

    Add Csv Helper Test

---
+++ tests/CodeBeam.MudBlazor.Extensions.UnitTests/Components/CsvMapperTests.cs
@@ -0,0 +1,25 @@
+﻿
+using AwesomeAssertions;
+using Bunit;
+
+namespace MudExtensions.UnitTests.Components
+{
+    [TestFixture]
+    public class CsvMapperTests : BunitTest
+    {
+        [Test]
+        public void MudCsvMapper_Should_Render_With_Minimal_Parameters()
+        {
+            var expectedHeaders = new List<MudExpectedHeader>
+            {
+                new("Id", required: true),
+                new("Name"),
+            };
+
+            var cut = Context.Render<MudCsvMapper>(parameters => parameters.Add(p => p.ExpectedHeaders, expectedHeaders));
+
+            cut.Markup.Should().NotBeNullOrWhiteSpace();
+            cut.FindAll("input").Count.Should().BeGreaterThan(0);
+        }
+    }
+}
