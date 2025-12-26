2117fa0e0edab9b416c470df57fcf8b7e0736ab6
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2025-12-26 14:48:57 +0300

    Upgrade .NET installation to version 10.0.100

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
 chmod +x dotnet-install.sh 
-./dotnet-install.sh -c 9.0 --version 9.0.100 -InstallDir ./dotnet9 
-./dotnet9/dotnet --version 
-./dotnet9/dotnet tool install Excubo.WebCompiler --global
-./dotnet9/dotnet publish CodeBeam.MudBlazor.Extensions.Docs.Wasm/CodeBeam.MudBlazor.Extensions.Docs.Wasm.csproj -c Release -o output
+./dotnet-install.sh -c 10.0 --version 10.0.100 -InstallDir ./dotnet10 
+./dotnet10/dotnet --version
+./dotnet10/dotnet publish CodeBeam.MudBlazor.Extensions.Docs.Wasm/CodeBeam.MudBlazor.Extensions.Docs.Wasm.csproj -c Release -o output
