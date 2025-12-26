3478f1b1b4c743be08d65b2db06367c1d2d83ab5
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2025-12-26 21:36:23 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
-chmod +x dotnet-install.sh 
-./dotnet-install.sh -c 10.0 --version 10.0.100 -InstallDir ./dotnet10 
-./dotnet10/dotnet --version
-./dotnet10/dotnet publish CodeBeam.MudBlazor.Extensions.Docs.Wasm/CodeBeam.MudBlazor.Extensions.Docs.Wasm.csproj -c Release -o output
