80d5cf9fc151741de12b5bcef896a9b9c78bba1e
Mehmet Can Karagöz <m.cankaragoz@outlook.com>
2026-02-17 01:46:03 +0300

    Try Create CodeViewer

---
+++ src/CodeBeam.MudBlazor.Extensions.Code/wwwroot/MudCode.js
@@ -0,0 +1,20 @@
+﻿window.CodeBeamCode = {
+
+    highlight: function (element) {
+        if (window.Prism) {
+            Prism.highlightElement(element);
+        }
+    },
+
+    copy: function (text) {
+        navigator.clipboard.writeText(text);
+    },
+
+    debounce: function (func, delay) {
+        let timer;
+        return function () {
+            clearTimeout(timer);
+            timer = setTimeout(() => func.apply(this, arguments), delay);
+        };
+    }
+};
